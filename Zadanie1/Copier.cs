3b16c93e07e2e9fa6669890c926e8d3ea3741ba5
Jakub Hojda <jakub.hojda@microsoft.wsei.edu.pl>
2023-05-04 13:10:05 +0200

    base

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,40 @@
+﻿using ver1;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public new int Counter { get; private set; } = 0;
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+
+        public new void PowerOn()
+        {
+            state = IDevice.State.on;
+            Counter++;
+        }
+        public new void PowerOff()
+        {
+            state = IDevice.State.off;
+        }
+
+        public void Print(in IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            throw new NotImplementedException();
+        }
+        public void Scan(out IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScanAndPrint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Zadanie1UT/UnitTestCopier.cs
+++ Zadanie1UT/UnitTestCopier.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ver1;
+using Zadanie1;
 using System;
 using System.IO;
 
